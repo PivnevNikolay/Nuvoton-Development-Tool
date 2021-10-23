5305bf4a2b9891ee386ceadf3fa3be103489af03
unknown <skuratowaanna@yandex.ru>
2021-10-23 23:27:04 +0300

    new repository

---
+++ FreeRTOS_NuMaker-M031TC/001_LED_Blinking_PB14/LED_Blinking_PB14_main.c
@@ -0,0 +1,66 @@
+/*
+23.10.2021
+Blinking LED PB14...
+*/
+#include <stdio.h>
+#include "NuMicro.h"
+/* Kernel includes. */
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "semphr.h"
+
+void SYS_Init(void)
+{
+		SYS_UnlockReg();
+ /* Enable HIRC clock (Internal RC 48MHz) */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+    /* Wait for HIRC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+    /* Select HCLK clock source as HIRC and HCLK source divider as 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+    /* Enable UART0 clock */
+    CLK_EnableModuleClock(UART0_MODULE);
+    /* Switch UART0 clock source to HIRC */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+    /* Update System Core Clock */
+    SystemCoreClockUpdate();
+    /* Set PB multi-function pins for UART0 RXD=PB.12 and TXD=PB.13 */
+    SYS->GPB_MFPH = (SYS->GPB_MFPH & ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk))    |       \
+                    (SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+		SYS_LockReg(); 
+}
+
+void UART0_Init()
+{
+    SYS_ResetModule(UART0_RST);
+    UART_Open(UART0, 115200);
+}
+
+void LED_Task(void *arg)
+{
+    (void)arg;
+	GPIO_SetMode(PB, BIT14, GPIO_MODE_OUTPUT);    
+	while(1) {
+	GPIO_TOGGLE(PB14); 
+	printf("LED_Task_running\n");		
+    vTaskDelay(1000);
+	}
+}
+
+int main()
+{	  
+    SYS_Init();    
+    UART0_Init();
+    printf("\nLED Blink_FreeRTOS Sample Program\n");
+/*---------- Create LED task, which will blink the RED LED PB14------------*/
+    xTaskCreate(
+        LED_Task,       /* Task function */
+        "LED Blink",    /* Task name (string) */
+        40,            /* Task stack, allocated from heap */
+        0,              /* No param passed to task function */
+        1,              /* Low priority */
+        0 );            /* Not using the task handle */
+    vTaskStartScheduler();
+    while(1);
+}
