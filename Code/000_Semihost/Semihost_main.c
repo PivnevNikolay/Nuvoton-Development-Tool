35b726a50b8e6babfb98ca3683ce2dcd109a1cb7
unknown <pivnev_tosno@mail.ru>
2022-05-04 17:59:43 +0300

    edited the repository

--- Code/000_Semihost/Semihost_main.c
+++ Code/000_Semihost/Semihost_main.c
@@ -37,31 +37,31 @@ int32_t main()
   SYS_Init();
   Float_transform(Val, 6, &sign_number, &integer_number, &fractional_number);//шесть знаков после запятой
   printf("Val = %d.%.06ld\r\n", integer_number, fractional_number);//выводим шесть знаков после запятой
-	Float_transform(Val, 4, &sign_number, &integer_number, &fractional_number);//четыре знака после запятой
-	printf("Val = %d.%.04ld\r\n", integer_number, fractional_number);//выводим четыре знака после запятой
-	Float_transform(Val, 1, &sign_number, &integer_number, &fractional_number);//один знак после запятой
-	printf("Val = %d.%.01ld\r\n", integer_number, fractional_number);//выводим один знак после запятой 
-	printf("\nStart SEMIHOST test: \n");
+  Float_transform(Val, 4, &sign_number, &integer_number, &fractional_number);//четыре знака после запятой
+  printf("Val = %d.%.04ld\r\n", integer_number, fractional_number);//выводим четыре знака после запятой
+  Float_transform(Val, 1, &sign_number, &integer_number, &fractional_number);//один знак после запятой
+  printf("Val = %d.%.01ld\r\n", integer_number, fractional_number);//выводим один знак после запятой 
+  printf("\nStart SEMIHOST test: \n");
   while(1)
   {     
-		item = getchar();
-    printf("%c\n",item);     
+  item = getchar();
+  printf("%c\n",item);     
   }
 }
 
 void Float_transform(float value, uint8_t width, uint8_t *sign_number, int *integer_number, uint32_t *fractional_number) {
-	uint8_t i;
-	uint32_t rounding_2 = 1;
-	float rounding = 0.5f;
-	*sign_number = 0;
-	if (*((uint32_t*) &value) & 0x80000000) {
-		*((uint32_t*) &value) &= 0x7FFFFFFF;
-		*sign_number = 1;
-	}
-	for (i = 0; i < width; i++) {
-		rounding_2 = rounding_2 * 10;
-	}
-	rounding = rounding / rounding_2;
-	*integer_number = (int) ((float) value + rounding);
-	*fractional_number = (((float) value + rounding) * rounding_2) - (*integer_number * rounding_2);
+  uint8_t i;
+  uint32_t rounding_2 = 1;
+  float rounding = 0.5f;
+  *sign_number = 0;
+  if (*((uint32_t*) &value) & 0x80000000) {
+  *((uint32_t*) &value) &= 0x7FFFFFFF;
+  *sign_number = 1;
+  }
+    for (i = 0; i < width; i++) {
+    rounding_2 = rounding_2 * 10;
+  }
+  rounding = rounding / rounding_2;
+  *integer_number = (int) ((float) value + rounding);
+  *fractional_number = (((float) value + rounding) * rounding_2) - (*integer_number * rounding_2);
 }
