03faab410bb6e867b873ac5bcd69de3f0c68e89a
unknown <pivnev_tosno@mail.ru>
2022-06-02 11:56:14 +0300

    edited the repository

--- Code/006_NuEclipse_Hello_world/main_Hello_world.c
+++ Code/006_NuEclipse_Hello_world/main_Hello_world.c
@@ -1,54 +1,86 @@
-/**-----------------------------------------------------------------------------------------------
-*\date  29.05.2022
-*\brief 
-*\authors ScuratovaAnna + PivnevNikolay
-* Ссылка на Telegram канал: https://t.me/nuvoton_programming
-*/
-#include <stdio.h>
+#include "stdio.h"
 #include "NuMicro.h"
-#define PLL_CLOCK           192000000
+#define Led  PB0
 void SYS_Init(void)
 {
     /*---------------------------------------------------------------------------------------------------------*/
     /* Init System Clock                                                                                       */
     /*---------------------------------------------------------------------------------------------------------*/
+
+    /* Unlock protected registers */
     SYS_UnlockReg();
-    /* Set XT1_OUT(PF.2) and XT1_IN(PF.3) to input mode */
-    PF->MODE &= ~(GPIO_MODE_MODE2_Msk | GPIO_MODE_MODE3_Msk);
-    /* Enable External XTAL (4~24 MHz) */
-    CLK_EnableXtalRC(CLK_PWRCTL_HXTEN_Msk);
-    /* Waiting for 12MHz clock ready */
-    CLK_WaitClockReady(CLK_STATUS_HXTSTB_Msk);
-    /* Set core clock as PLL_CLOCK from PLL */
-    CLK_SetCoreClock(PLL_CLOCK);
-    /* Set PCLK0/PCLK1 to HCLK/2 */
-    CLK->PCLKDIV = (CLK_PCLKDIV_APB0DIV_DIV2 | CLK_PCLKDIV_APB1DIV_DIV2);
+
+    /* Enable Internal RC 12MHz clock */
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+
+    /* Waiting for Internal RC clock ready */
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+
+    /* Switch HCLK clock source to Internal RC and HCLK source divide 1 */
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+
     /* Enable UART clock */
     CLK_EnableModuleClock(UART0_MODULE);
-    /* Select UART clock source from HXT */
-    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HXT, CLK_CLKDIV0_UART0(1));
+    CLK_EnableModuleClock(TMR0_MODULE);
+
+    /* Select UART clock source from HIRC */
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+    CLK_SetModuleClock(TMR0_MODULE, CLK_CLKSEL1_TMR0SEL_PCLK0, 0);
+
+    /* Update System Core Clock */
+    /* User can use SystemCoreClockUpdate() to calculate SystemCoreClock. */
     SystemCoreClockUpdate();
-    /* Set GPB multi-function pins for UART0 RXD and TXD */
-    SYS->GPB_MFPH &= ~(SYS_GPB_MFPH_PB12MFP_Msk | SYS_GPB_MFPH_PB13MFP_Msk);
-    SYS->GPB_MFPH |= (SYS_GPB_MFPH_PB12MFP_UART0_RXD | SYS_GPB_MFPH_PB13MFP_UART0_TXD);
+
+
+    /*---------------------------------------------------------------------------------------------------------*/
+    /* Init I/O Multi-function                                                                                 */
+    /*---------------------------------------------------------------------------------------------------------*/
+   // Uart0DefaultMPF();
+    SYS->GPA_MFPH = (SYS->GPA_MFPH & ~SYS_GPA_MFPH_PA15MFP_Msk) | SYS_GPA_MFPH_PA15MFP_UART0_RXD;
+    SYS->GPA_MFPH = (SYS->GPA_MFPH & ~SYS_GPA_MFPH_PA14MFP_Msk) | SYS_GPA_MFPH_PA14MFP_UART0_TXD ;
+
+
     /* Lock protected registers */
     SYS_LockReg();
 }
 
+void UART0_Init()
+{
     /*---------------------------------------------------------------------------------------------------------*/
-    /* This is a template project for M480 series MCU.                                                         */
-    /* This template application uses external crystal as HCLK source and configures UART0 to print out        */
-    /* "Hello World", SystemCoreClock, CLK_GetHXTFreq                                                          */
+    /* Init UART                                                                                               */
     /*---------------------------------------------------------------------------------------------------------*/
+    /* Reset UART module */
+    //SYS_ResetModule(UART0_RST);
 
+    /* Configure UART0 and set UART0 baud rate */
+    UART_Open(UART0, 115200);
+}
+
+/*---------------------------------------------------------------------------------------------------------*/
+/*  Main Function                                                                                          */
+/*---------------------------------------------------------------------------------------------------------*/
 int main()
 {
+	 /* Init System, IP clock and multi-function I/O. */
     SYS_Init();
-    UART_Open(UART0, 115200);
-    printf("Hello World\n");
-    printf("CPU -> %d Hz\n", SystemCoreClock);
-    uint32_t GetHXT;
-    GetHXT=CLK_GetHXTFreq();
-    printf("HXTEN -> %d Hz\n", GetHXT);
-    while(1);
+
+   /* Init UART0 for printf */
+    UART0_Init();
+
+
+ //PB->MODE = (PB->MODE & ~(GPIO_MODE_MODE14_Msk)) | (GPIO_MODE_OUTPUT << GPIO_MODE_MODE14_Pos);
+    GPIO_SetMode(PB, BIT0, GPIO_MODE_OUTPUT);
+    printf("\n Start SEMIHOST test: \n");
+    while(1){
+    GPIO_TOGGLE(Led);
+    printf("System clock rate: %d Hz \n\r", SystemCoreClock);
+    printf("Led ON\n\r");
+    TIMER_Delay(TIMER0, 0xF4240);//Delay 1 second
+    //CLK_SysTickDelay(349525);
+    GPIO_TOGGLE(Led);
+    printf("CPU Freq rate: %d Hz \n\r", CLK_GetCPUFreq());
+    printf("Led OFF\n\r");
+    TIMER_Delay(TIMER0, 0xF4240);//Delay 1 second
+    //CLK_SysTickDelay(349525);
+  }
 }
