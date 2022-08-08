8d7826b8fe831aa410762664889113f1730198cb
unknown <pivnev_tosno@mail.ru>
2022-08-08 19:03:55 +0300

    edited the repository

--- Code/002_GPIO_NuEclipse/GPIO_OUT_7-SEGMENT_LED/main.c
+++ Code/002_GPIO_NuEclipse/GPIO_OUT_7-SEGMENT_LED/main.c
@@ -1,6 +1,6 @@
 /**---------------------------------------------
-*\date 
-*\brief 
+*\date
+*\brief
 *      1        1->PA.0
 *     ---       2->PA.1
 *  6 | 7 |2     3->PA.2
@@ -10,24 +10,75 @@
 *               7->PA.6
 *\authors ScuratovaAnna + PivnevNikolay
 */
+//------------------------------------1----------------------------------//
 #include <stdio.h>
 #include "NuMicro.h"
 #include "system_init.h"
 ///               0    1   2    3    4    5    6    7   8    9
 uint8_t led[10]={0x3F,0x6,0x5B,0x4F,0x66,0x6D,0x7D,0x7,0x7F,0x6F};
-//---------------------------------------//
+//-----------------------------------------------------------------------//
 int main()
 {
   SYS_Init();
   UART0_Init();
   GPIO_SetMode(PA, (BIT0 | BIT1 | BIT2 | BIT3 | BIT4 | BIT5 | BIT6 ), GPIO_MODE_OUTPUT);
   printf("\n\nCPU @ %u Hz\n", SystemCoreClock);
-//---------------------------------------//
+//-----------------------------------------------------------------------//
   while(1){
    for ( uint8_t z =0;z<10;z++){
-   PA->DOUT = led[z];
+   //PA->DOUT = led[z];
+   PA->DOUT = *(led+z);
    printf("z = %u \n",z);
    CLK_SysTickDelay(349525);
   }
  }
 }
+//-------------------------------------END-------------------------------//
+//---------------------------------------2-------------------------------//
+#include <stdio.h>
+#include "NuMicro.h"
+#include "system_init.h"
+int main()
+{
+  SYS_Init();
+  UART0_Init();
+  PA->MODE = (PA->MODE & ~(GPIO_MODE_MODE0_Msk | GPIO_MODE_MODE1_Msk | GPIO_MODE_MODE2_Msk |
+	 GPIO_MODE_MODE3_Msk | GPIO_MODE_MODE4_Msk | GPIO_MODE_MODE5_Msk | GPIO_MODE_MODE6_Msk)) |
+	(GPIO_MODE_OUTPUT << GPIO_MODE_MODE0_Pos | GPIO_MODE_OUTPUT << GPIO_MODE_MODE1_Pos | GPIO_MODE_OUTPUT << GPIO_MODE_MODE2_Pos |
+	 GPIO_MODE_OUTPUT << GPIO_MODE_MODE3_Pos | GPIO_MODE_OUTPUT << GPIO_MODE_MODE4_Pos | GPIO_MODE_OUTPUT << GPIO_MODE_MODE5_Pos |
+	 GPIO_MODE_OUTPUT << GPIO_MODE_MODE6_Pos);
+  printf("\n\nCPU @ %u Hz\n", SystemCoreClock);
+//---------------------------------------//
+  while(1){
+   for ( uint8_t z =0;z<5;z++){
+   switch(z){
+       case 0:
+    PA->DOUT &= ~((1 << 0)|(1 << 1)|(1 << 2)|(1 << 3)|(1 << 4)|(1 << 5)|(1<<6));
+    PA->DOUT|= (1 << 0)|(1 <<1)|(1 << 2)|(1 << 3)|(1 << 4)|(1 <<5);//0
+    CLK_SysTickDelay(349525);
+       break;
+       case 1:
+    PA->DOUT &= ~((1 << 0)|(1 << 1)|(1 << 2)|(1 << 3)|(1 << 4)|(1 << 5)|(1<<6));
+    PA->DOUT |= (1 <<1)|(1 << 2);//1
+    CLK_SysTickDelay(349525);
+       break;
+       case 2:
+    PA->DOUT &= ~((1 << 0)|(1 << 1)|(1 << 2)|(1 << 3)|(1 << 4)|(1 << 5)|(1<<6));
+    PA->DOUT |= (1 << 0)|(1 <<1)|(1 << 3)|(1 << 4)|(1<<6);//2
+    CLK_SysTickDelay(349525);
+       break;
+       case 3:
+    PA->DOUT &= ~((1 << 0)|(1 << 1)|(1 << 2)|(1 << 3)|(1 << 4)|(1 << 5)|(1<<6));
+    PA->DOUT  |= (1 << 0)|(1 <<1)|(1 << 2)|(1 << 3)| (1<<6);//3
+    CLK_SysTickDelay(349525);
+       break;
+       case 4:
+    PA->DOUT &= ~((1 << 0)|(1 << 1)|(1 << 2)|(1 << 3)|(1 << 4)|(1 << 5)|(1<<6));
+    PA->DOUT |= (1 << 1)|(1 <<2)|(1 << 5)|(1<<6);//4
+    CLK_SysTickDelay(349525);
+       break;
+     }
+  }
+ }
+}
+//-------------------------------------END-------------------------------//
--- Code/002_GPIO_NuEclipse/GPIO_OUT_7-SEGMENT_LED/system_init.c
+++ Code/002_GPIO_NuEclipse/GPIO_OUT_7-SEGMENT_LED/system_init.c
@@ -4,27 +4,33 @@
 
 void SYS_Init(void)
 {
-  SYS_UnlockReg();
-  // Enable HIRC clock (Internal RC 48MHz)
-  CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
-  // Wait for HIRC clock ready
-  CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
-  // Select HCLK clock source as HIRC and HCLK source divider as 1
-  CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
-  // Enable UART0 clock
-  CLK_EnableModuleClock(UART0_MODULE);
-  CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
-  SystemCoreClockUpdate();
-  // Set multi-function pins UART0 RXD = PD.2 and TXD = PA.7
-  SYS->GPD_MFPL = (SYS->GPD_MFPL & ~SYS_GPD_MFPL_PD2MFP_Msk) | SYS_GPD_MFPL_PD2MFP_UART0_RXD;
-  SYS->GPA_MFPL = (SYS->GPA_MFPL & ~SYS_GPA_MFPL_PA7MFP_Msk) | SYS_GPA_MFPL_PA7MFP_UART0_TXD;
-  SYS_LockReg();
+    SYS_UnlockReg();
+    // Enable HIRC clock (Internal RC 48MHz)
+    CLK_EnableXtalRC(CLK_PWRCTL_HIRCEN_Msk);
+    // Wait for HIRC clock ready
+    CLK_WaitClockReady(CLK_STATUS_HIRCSTB_Msk);
+    // Select HCLK clock source as HIRC and HCLK source divider as 1
+    CLK_SetHCLK(CLK_CLKSEL0_HCLKSEL_HIRC, CLK_CLKDIV0_HCLK(1));
+    // Enable UART0 clock
+    CLK_EnableModuleClock(UART0_MODULE);
+    CLK_SetModuleClock(UART0_MODULE, CLK_CLKSEL1_UART0SEL_HIRC, CLK_CLKDIV0_UART0(1));
+    SystemCoreClockUpdate();
+    //Set PA.0 ~ PA.6 to GPIO
+    SYS->GPA_MFPL = (SYS->GPA_MFPL & ~(SYS_GPA_MFPL_PA0MFP_Msk |
+    SYS_GPA_MFPL_PA1MFP_Msk | SYS_GPA_MFPL_PA2MFP_Msk | SYS_GPA_MFPL_PA3MFP_Msk | SYS_GPA_MFPL_PA4MFP_Msk |
+	SYS_GPA_MFPL_PA5MFP_Msk | SYS_GPA_MFPL_PA6MFP_Msk)) |
+    (SYS_GPA_MFPL_PA0MFP_GPIO | SYS_GPA_MFPL_PA1MFP_GPIO | SYS_GPA_MFPL_PA2MFP_GPIO | SYS_GPA_MFPL_PA3MFP_GPIO |
+    SYS_GPA_MFPL_PA4MFP_GPIO | SYS_GPA_MFPL_PA5MFP_GPIO | SYS_GPA_MFPL_PA6MFP_GPIO);
+    // Set multi-function pins UART0 RXD = PD.2 and TXD = PA.7
+    SYS->GPD_MFPL = (SYS->GPD_MFPL & ~SYS_GPD_MFPL_PD2MFP_Msk) | SYS_GPD_MFPL_PD2MFP_UART0_RXD;
+    SYS->GPA_MFPL = (SYS->GPA_MFPL & ~SYS_GPA_MFPL_PA7MFP_Msk) | SYS_GPA_MFPL_PA7MFP_UART0_TXD;
+    SYS_LockReg();
 }
 
 void UART0_Init(void)
 {
-  // Reset UART0
-  SYS_ResetModule(UART0_RST);
-  // Configure UART0 and set UART0 baud rate
-  UART_Open(UART0, 115200);
+    // Reset UART0
+    SYS_ResetModule(UART0_RST);
+    // Configure UART0 and set UART0 baud rate
+    UART_Open(UART0, 115200);
 }
